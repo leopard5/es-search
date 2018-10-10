000c50a4b82f70455ab9d3099e7840cc9ca45c1f
qiyazhong <qiyazhong@163.com>
2018-10-10 15:10:29 +0800

    consts modify

--- es-data-push/src/main/java/com/yz/search/constant/RedisConsts.java
+++ es-data-push/src/main/java/com/yz/search/constant/RedisConsts.java
@@ -4,9 +4,8 @@ package com.yz.search.constant;
  * @author yazhong
  */
 public class RedisConsts {
-    private RedisConsts(){}
 
     public static final String REDIS_DELIMETER = ":";
     public static final String LOCK = "lock:";
-    public static final String TC_PREFIX = "tc:";
+    public static final String PROJECT_ES_ABBREVIATION = "es:";
 }
--- es-data-push/src/main/java/com/yz/search/constant/ZKConsts.java
+++ es-data-push/src/main/java/com/yz/search/constant/ZKConsts.java
@@ -1,4 +1,5 @@
 package com.yz.search.constant;
 
 public class ZKConsts {
+    public static final String ZK_PATH_DELIMETER = "/";
 }
